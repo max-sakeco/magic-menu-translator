94eff9fdf2b000905e55a0ac39f050e2b32ef0a5
maxdelvita-ms <max@thesakecompany.com>
2025-01-20 10:17:57 +0800

    Fix for Vercel

--- app/page.tsx
+++ app/page.tsx
@@ -34,6 +34,7 @@ export default function Home() {
     setMenuPhotos(data.photos);
   }, []);
 
+  
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
       <main className="container mx-auto px-4 py-12">
